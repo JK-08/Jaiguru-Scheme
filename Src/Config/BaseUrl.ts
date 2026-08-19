2d45a8ca0b4398d8cf8ab08c165b5f0403d1d3c0
FIREJADDU-08 <kirubajadu08@gmail.com>
2026-08-19 09:40:46 +0530

    Update BaseUrl.ts

--- Src/Config/BaseUrl.ts
+++ Src/Config/BaseUrl.ts
@@ -14,5 +14,5 @@ export const IMAGE_BASE_URL = 'https://scheme.jaigurujewellers.com';
 // export const MPIN_API_BASE_URL = 'http://192.168.0.21:8104/api/v1/mpin';
 
 // Android Emulator
-// export const API_BASE_URL = 'https://10.0.2.2:8104/api/v1';
-// export const MPIN_API_BASE_URL = 'https://10.0.2.2:8104/api/v1/mpin';
+// export const API_BASE_URL = 'http://10.0.2.2:8104/api/v1';
+// export const MPIN_API_BASE_URL = 'http://10.0.2.2:8104/api/v1/mpin';
